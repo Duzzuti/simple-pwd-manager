4f2119c3db6382d2e6af3b0117fe0c28499d51b8
Duzzuti <duzzuti@gmail.com>
2024-09-06 22:58:03 +0200

    add other information + enhance input handling

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ def encrypt_pdata(data: bytes, output_file: str, password: str):
 
 if __name__ == "__main__":
     password = None
-    data = pickle.dumps(pd.DataFrame({"Website": [], "Email": [], "Username": [], "Password": []}))
+    data = pickle.dumps([pd.DataFrame({"Website": [], "Email": [], "Username": [], "Password": []}), pd.DataFrame({"Name": [], "Info": []})])
     if not os.path.isfile(file):
         user_new_input = easygui.ynbox("No encrypted password file found. Do you want to create a new one?", "Password Manager")
         if user_new_input:
@@ -116,16 +116,17 @@ if __name__ == "__main__":
             data = decrypt_file(file, password)
         data = data[len(correct_bytes):]
     
-    df : pd.DataFrame = pickle.loads(data)
+    df_pwd : pd.DataFrame = pickle.loads(data)[0]
+    df_info : pd.DataFrame = pickle.loads(data)[1]
 
     while True:
-        user_choice = easygui.buttonbox("Choose an action: ", "Password Manager", ["Show passwords", "Add password", "Delete/Change Password", "Exit"])
+        user_choice = easygui.buttonbox("Choose an action: ", "Password Manager", ["Show passwords", "Add password", "Show other", "Add other", "Delete/Change Password", "Exit"])
         if user_choice == "Show passwords":
             # print full df without index sorted by website
-            if len(df) == 0:
+            if len(df_pwd) == 0:
                 easygui.msgbox("No websites found.", "Password Manager")
                 continue
-            print(df.sort_values(by="Website").to_string(index=False))
+            print(df_pwd.sort_values(by="Website").to_string(index=False))
             input("Press Enter to clear...")
             os.system('cls' if os.name == 'nt' else 'clear')
         elif user_choice == "Add password":
@@ -136,37 +137,80 @@ if __name__ == "__main__":
                 elif data_addpwd[0] == "" or data_addpwd[3] == "":
                     easygui.msgbox("A website and a password is required.", "Password Manager")
                 else:
+                    if data_addpwd[0] in df_pwd["Website"].values:
+                        # get all emails and usernames as strings
+                        emails = df_pwd[df_pwd["Website"] == data_addpwd[0]]["Email"].values
+                        usernames = df_pwd[df_pwd["Website"] == data_addpwd[0]]["Username"].values
+                        emails_plus_usernames = "\n".join(["Mail:" + emails[i] + ", User:" + usernames[i] for i in range(len(emails))])
+
+                        user_confirm = easygui.ynbox("The website already exists. Do you want to add the information as an other login? (No does NOT safe the new information). \n" + \
+                                                    "If you want to change old information (e.g. a single password) please use the 'Change password' section of the main menu.\n\n" + \
+                                                    "Current information for " + data_addpwd[0] + ":\n" +
+                                                    emails_plus_usernames + \
+                                                    "\nNew information:\n" + \
+                                                    "Mail:" + data_addpwd[1] + ", User:" + data_addpwd[2] + "\n", "Password Manager")
+                        if not user_confirm:
+                            continue
+                    df_pwd = pd.concat([df_pwd, pd.DataFrame({"Website": [data_addpwd[0]], "Email": [data_addpwd[1]], "Username": [data_addpwd[2]], "Password": [data_addpwd[3]]})], ignore_index=True)
+                    encrypt_pdata(pickle.dumps([df_pwd, df_info]), file, password)
                     break
-            if data_addpwd is not None:
-                df = pd.concat([df, pd.DataFrame({"Website": [data_addpwd[0]], "Email": [data_addpwd[1]], "Username": [data_addpwd[2]], "Password": [data_addpwd[3]]})], ignore_index=True)
-                encrypt_pdata(pickle.dumps(df), file, password)
+        # todo show other information in good format
+        elif user_choice == "Show other":
+            if len(df_info) == 0:
+                easygui.msgbox("No other information found.", "Password Manager")
+                continue
+            print(df_info.to_string(index=False))
+            input("Press Enter to clear...")
+            os.system('cls' if os.name == 'nt' else 'clear')
+
+        # todo handle multiple entries with the same name
+        elif user_choice == "Add other":
+            while True:
+                other_name = easygui.enterbox("Enter a name for the information you want to add. This is only for your reference.", "Password Manager")
+                if other_name is None:
+                    break
+                elif other_name == "":
+                    easygui.msgbox("A name is required.", "Password Manager")
+                    continue
+                while True:
+                    other_info = easygui.textbox("Store any information you want for " + other_name, "Password Manager") #Not empty
+                    if other_info is None:
+                        break
+                    elif other_info == "":
+                        easygui.msgbox("The information is required.", "Password Manager")
+                        continue
+                    else:
+                        df_info = pd.concat([df_info, pd.DataFrame({"Name": [other_name], "Info": [other_info]})], ignore_index=True)
+                        encrypt_pdata(pickle.dumps([df_pwd, df_info]), file, password)
+                        break
+                break
         elif user_choice == "Delete/Change Password":
             # if there is only one website, delete it
-            if len(df) == 0:
+            if len(df_pwd) == 0:
                 easygui.msgbox("No websites found.", "Password Manager")
                 continue
-            elif len(df) == 1:
-                choice_dc = [df["Website"].values[0]]
+            elif len(df_pwd) == 1:
+                choice_dc = [df_pwd["Website"].values[0]]
             else:
-                choice_dc = easygui.multchoicebox("Choose the websites to delete or change the information for: ", "Password Manager", df["Website"].sort_values().tolist())
+                choice_dc = easygui.multchoicebox("Choose the websites to delete or change the information for: ", "Password Manager", df_pwd["Website"].sort_values().tolist())
             if choice_dc is not None:
                 for i in choice_dc:
                     user_dc = easygui.buttonbox("Choose an action for " + i + ": ", "Password Manager", ["Delete", "Change Information", "Skip"])
                     if user_dc == "Skip" or user_dc is None:
                         continue
                     elif user_dc == "Delete":
-                        df = df[df["Website"] != i]
+                        df_pwd = df_pwd[df_pwd["Website"] != i]
                     elif user_dc == "Change Information":
                         while True:
-                            data_addpwd = easygui.multenterbox("Enter the following information: ", "Password Manager", ["Website", "Email", "Username", "Password"], [i, df[df["Website"] == i]["Email"].values[0], df[df["Website"] == i]["Username"].values[0], df[df["Website"] == i]["Password"].values[0]])
+                            data_addpwd = easygui.multenterbox("Enter the following information: ", "Password Manager", ["Website", "Email", "Username", "Password"], [i, df_pwd[df_pwd["Website"] == i]["Email"].values[0], df_pwd[df_pwd["Website"] == i]["Username"].values[0], df_pwd[df_pwd["Website"] == i]["Password"].values[0]])
                             if data_addpwd[0] == "" or data_addpwd[3] == "":
                                 easygui.msgbox("A website and a password is required.", "Password Manager")
                             else:
                                 break
                         if data_addpwd is not None:
-                            df = df[df["Website"] != i]
-                            df = pd.concat([df, pd.DataFrame({"Website": [data_addpwd[0]], "Email": [data_addpwd[1]], "Username": [data_addpwd[2]], "Password": [data_addpwd[3]]})], ignore_index=True)
-                encrypt_pdata(pickle.dumps(df), file, password)
+                            df_pwd = df_pwd[df_pwd["Website"] != i]
+                            df_pwd = pd.concat([df_pwd, pd.DataFrame({"Website": [data_addpwd[0]], "Email": [data_addpwd[1]], "Username": [data_addpwd[2]], "Password": [data_addpwd[3]]})], ignore_index=True)
+                encrypt_pdata(pickle.dumps([df_pwd, df_info]), file, password)
 
         elif user_choice == "Exit" or user_choice is None:
             break
